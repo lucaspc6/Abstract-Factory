240ec84a79af2186525a0e3c16336f8d790e6eb9
Lucas Carvalho <lucas.carvalho84@fatec.sp.gov.br>
2025-03-22 22:54:18 -0300

    Update Button.java

--- src/main/java/buttons/Button.java
+++ src/main/java/buttons/Button.java
@@ -1,15 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package buttons;
 
 /**
+ * A interface Button define um contrato para a criação de botões em uma interface gráfica.
+ * Qualquer classe que implemente esta interface deve fornecer sua própria implementação do método `paint()`,
+ * permitindo a personalização do desenho do botão conforme o estilo desejado (ex: Windows, macOS, Linux).
  *
- * @author FATEC ZONA LESTE
+ * Essa abordagem facilita a criação de componentes gráficos flexíveis e desacoplados do sistema operacional.
  */
-// Interface obriga as classes a criarem um método para desenhar o botão.
 public interface Button {
+    /**
+     * Método responsável por renderizar o botão na interface gráfica.
+     * Cada implementação desse método definirá a aparência e o comportamento do botão.
+     */
     void paint();
 }
 
